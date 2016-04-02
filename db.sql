8130daf0660a760e9da21457b8e0705087c9231b
Denis Yakovlev <ydenis@bk.ru>
2016-04-03 05:16:54 +0600

    New structure

--- db.sql
+++ db.sql
@@ -1,41 +1,23 @@
--- phpMyAdmin SQL Dump
--- version 4.0.10deb1
--- http://www.phpmyadmin.net
---
--- Хост: localhost
--- Время создания: Мар 31 2016 г., 10:13
--- Версия сервера: 5.5.47-0ubuntu0.14.04.1
--- Версия PHP: 5.5.33-1+deb.sury.org~trusty+1
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
---
--- База данных: `microservices`
---
 CREATE DATABASE IF NOT EXISTS `microservices` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `microservices`;
 
--- --------------------------------------------------------
-
---
--- Структура таблицы `news`
---
-
+DROP TABLE IF EXISTS `news`;
 CREATE TABLE IF NOT EXISTS `news` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `topic` varchar(50) NOT NULL,
   `text` text NOT NULL,
-  `image` text,
-  `time` text NOT NULL,
+  `image` varchar(50) DEFAULT NULL,
+  `time` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=13 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
