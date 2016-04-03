0e12ca4eb54b1fa71f4317bf6ac56bbacea606f4
Denis Yakovlev <ydenis@bk.ru>
2016-04-04 01:36:47 +0600

    Added an output stream for the http server

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ php bin/image-handler.php 127.0.0.1:5500 127.0.0.1:5700 127.0.0.1:5400 &
 php bin/text-handler.php 127.0.0.1:5500 127.0.0.1:5800 &
 php bin/publish-news.php 127.0.0.1:5500 127.0.0.1:5900 127.0.0.1:5910 &
 cd ./http
-php -S 127.0.0.1:5400 &
+php -S 127.0.0.1:5400 > /dev/null &
 cd ../
